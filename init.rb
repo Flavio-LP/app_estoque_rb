a5d1d230c9720b3d12da2b8fd66effc9d8ac54ee
Flavio-LP <flavio5070@outlook.com>
2025-03-14 10:19:14 -0300

    finalizado implementação de remoção de saldo

--- init.rb
+++ init.rb
@@ -1,7 +1,22 @@
 require_relative 'display/menu'
 require_relative 'display/style'
 
-produtos = []
+produtos = [
+    {
+        id: 1,
+        nome: "Maça",
+        descricao: "Maça da turma da mônica",
+        preco: 2.5,
+        quantidade: 20
+    },
+    {
+        id: 2,
+        nome: "Banana",
+        descricao: "Nanica",
+        preco: "1.5",
+        quantidade: 30
+    }
+]
 
 menu(produtos)
 
